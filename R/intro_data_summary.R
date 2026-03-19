08376d6f6fa42fc1b5d1b1db525644e990512faf
Katie Bunge <katarinabunge@gmail.com>
2026-03-19 11:44:06 -0700

    Made changes to intro data summary and exported clean data set

--- R/intro_data_summary.R
+++ R/intro_data_summary.R
@@ -23,3 +23,43 @@ summary(raw_15min)
 glimpse(raw_15min)
 
 dfSummary(raw_15min)
+
+#Call a specific column or row 
+#Call a row
+raw_15min[3,]
+#Call a column
+raw_15min[,2]
+
+
+#Mean 
+mean(raw_15min$speed)
+mean(raw_15min$volume)
+
+#Histogram
+hist(raw_15min$volume)
+
+#learn about filters 
+#filtering is dplyr function
+#introduce pipe function 
+#introduce filtering 
+
+occ_20plus <- raw_15min |> 
+  filter(occupancy > 20)
+occ10_sp80 <- raw_15min |> 
+  filter(occupancy < 10 & speed > 80)
+table(occ10_sp80$detector_id)
+
+# Working with null values also called NA's
+blank_example <- raw_15min |> 
+  filter(is.na(speed))
+# create a data frame excluding the NA values in the Speed variable 
+complete_df <- raw_15min |> 
+  filter(!is.na(speed))
+
+subset_o10s80 <- occ10_sp80 |> 
+  filter(detector_id %in% c(101185, 101176))
+
+
+#RDS is compressed data file specifically for working with R 
+#When you load data frame or table back in, it retains the formatting you did 
+saveRDS(complete_df, "data/clean_data.rds")
